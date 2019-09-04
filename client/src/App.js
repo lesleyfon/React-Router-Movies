1e25fe494c6fefd824ae31f8fec48932cacde1d0
lesley <lesleybanadzem2017@gmail.com>
2019-09-03 18:38:02 -0700

    Installed Server.js Dependencies and Imported the Route Component into App.js and renders Movie component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+import { Route } from 'react-router-dom'
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList'
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -8,11 +10,15 @@ const App = () => {
   const addToSavedList = movie => {
     setSavedList( [...savedList, movie] );
   };
-
   return (
     <div>
       <SavedList list={savedList} />
       <div>Replace this Div with your Routes</div>
+      <Route path='/' component = {MovieList}/>
+      <Route 
+        exact
+        path='/movies/:movieId' 
+        component = {MovieList} />
     </div>
   );
 };
